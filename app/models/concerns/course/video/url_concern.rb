57a7d953f178aedbc9b3eed3c8e4c210721ee3bb
wxupjack <mrcjava@gmail.com>
2023-02-28 14:30:58 +0800

    modify video compoent to china website

--- app/models/concerns/course/video/url_concern.rb
+++ app/models/concerns/course/video/url_concern.rb
@@ -11,20 +11,31 @@ module Course::Video::UrlConcern
     /(?:https?:\/\/)?youtu\.be\/(.+)/,
     /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=(.*?)(&|#|$)/,
     /(?:https?:\/\/)?(?:www\.)?youtube\.com\/embed\/(.*?)(\?|$)/,
-    /(?:https?:\/\/)?(?:www\.)?youtube\.com\/v\/(.*?)(#|\?|$)/
+    /(?:https?:\/\/)?(?:www\.)?youtube\.com\/v\/(.*?)(#|\?|$)/,
+    /(?:https?:\/\/)?(?:player\.)?youku\.com\/embed\/(.*?)(\?|$)/,
+
+    /(?:https?:\/\/)?(?:www\.)?bilibili\.com\/video\/(.*?)\/(.*?)/,
+    # https://www.bilibili.com/video/BV1us4y177NQ/?share_source=copy_web&vd_source=5b8554b44386faeead1291ad095c4a0b
+    /(?:https?:\/\/)?(?:player\.)?bilibili\.com\/player\.html\?(?:.*)bvid=(.*?)(&|#|$)/,
+    # http://player.bilibili.com/player.html?aid=950238715&bvid=BV1us4y177NQ&cid=1028851239&page=1
   ].freeze
 
   private
 
   # Changes the provided youtube URL to an embedded URL for display of videos.
   def convert_to_embedded_url
+    puts "***************************** " + url
     youtube_id = youtube_video_id_from_link(url)
+    puts "***************************** " + youtube_id
     self.url = youtube_embedded_url(youtube_id) if youtube_id
+    puts "***************************** " + self.url
   end
 
   # Default embedded youtube url for rendering in an iframe.
   def youtube_embedded_url(video_id)
-    "https://www.youtube.com/embed/#{video_id}"
+    # "https://www.youtube.com/embed/#{video_id}"
+    # "https://player.youku.com/embed/#{video_id}"
+    "https://player.bilibili.com/player.html?bvid=#{video_id}"
   end
 
   # Extracts the video ID from the yout
